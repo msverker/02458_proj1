66f7ae18093157b68c2cd5c4971a051568294aeb
Mads Sverker <madssverker@Madss-MacBook-Pro.local>
2025-09-18 22:16:52 +0200

    box_plotter code added

--- box_plotter.py
+++ box_plotter.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import spearmanr
+import numpy as np
 
 label_csv = Path("labels.csv")
 
@@ -18,8 +20,15 @@ def plot_boxplot(csv_path: Path) -> None:
     plt.tight_layout()
     plt.show()
 
-
+def calculate_spearman_correlation(csv_path):
+    pred_labels = np.array([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5])
+    df = pd.read_csv(csv_path, delimiter=';')
+    true_labels = df.iloc[:, 1:].mean(axis=1)
+    rho, pval = spearmanr(pred_labels, true_labels)
+    print(f"Spearman's rho = {rho:.3f}, p-value = {pval:.3g}")
 
 
 if __name__ == "__main__":
-    plot_boxplot(label_csv)
+    # plot_boxplot(label_csv)
+    csv_file = Path("labels_merged.csv")
+    calculate_spearman_correlation(csv_file)
